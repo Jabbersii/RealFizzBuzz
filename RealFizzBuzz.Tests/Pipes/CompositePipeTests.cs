c06044637e4ce15af97ed675e7d9eaf1e17ad972
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-06 20:28:49 +0100

    Added tests for pipes

---
+++ RealFizzBuzz.Tests/Pipes/CompositePipeTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RealFizzBuzz.Pipes;
+using Xunit;
+using System.Linq;
+
+namespace RealFizzBuzz.Tests.Pipes
+{
+    public class CompositePipeTests
+    {
+        [Fact]
+        public void No_pipes_ctor_throws_exception()
+        {
+            Assert.Throws<ArgumentException>(() => new CompositePipe());
+        }
+
+        [Fact]
+        public void True_only_pipes_returns_true_on_check()
+        {
+            var pipe = new CompositePipe(new TruePipe(), new TruePipe(), new TruePipe());
+
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                Assert.True(pipe.Check(i));
+            }
+        }
+
+        [Fact]
+        public void False_only_pipes_returns_false_on_check()
+        {
+            var pipe = new CompositePipe(new TruePipe(), new TruePipe(), new TruePipe());
+
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                Assert.False(pipe.Check(i));
+            }
+        }
+
+        [Fact]
+        public void False_and_true_pipes_returns_false_on_check()
+        {
+            var pipe = new CompositePipe(new TruePipe(), new FalsePipe(), new TruePipe());
+
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                Assert.False(pipe.Check(i));
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ConcatPipes))]
+        public void Pipe_returns_concatenation_of_composed_pipes(IPipe[] pipes, string expected)
+        {
+            var pipe = new CompositePipe(new TruePipe(), new FalsePipe());
+            Assert.Equal(expected, pipe.Process(0));
+        }
+
+        public static IEnumerable<object[]> ConcatPipes => new[]
+        {
+            new object[]{ new IPipe[] { new TruePipe(), new FalsePipe() }, "truefalse" },
+            new object[]{ new IPipe[] { new FalsePipe(), new TruePipe() }, "falsetrue" },
+            new object[]{ new IPipe[] { new FalsePipe(), new FalsePipe() }, "falsefalse" },
+            new object[]{ new IPipe[] { new TruePipe(), new TruePipe() }, "truetrue" }
+        };
+
+        class TruePipe : IPipe
+        {
+            public bool Check(int number) => true;
+            public string Process(int number) => "true";
+        }
+
+        class FalsePipe : IPipe
+        {
+            public bool Check(int number) => false;
+            public string Process(int number) => "false";
+        }
+    }
+}
---
+++ RealFizzBuzz.Tests/Pipes/NumberSubstitutePipeTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RealFizzBuzz.Pipes;
+using Xunit;
+
+namespace RealFizzBuzz.Tests.Pipes
+{
+    public class NumberSubstitutePipeTests
+    {
+        [Theory]
+        [MemberData(nameof(CheckTestData))]
+        public void Number_substitute_check_passes_for_expected_numbers(int target, int input, bool expected)
+        {
+            IPipe pipe = new NumberSubstitutePipe(target, string.Empty);
+
+            bool actual = pipe.Check(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> CheckTestData => new[]
+            {
+                new object[] { 3, 1, false },
+                new object[] { 3, 2, false },
+                new object[] { 3, 3, true  },
+                new object[] { 3, 4, false },
+                new object[] { 5, 1, false },
+                new object[] { 5, 2, false },
+                new object[] { 5, 3, false },
+                new object[] { 5, 4, false },
+                new object[] { 5, 5, true  }
+            };
+    }
+}
