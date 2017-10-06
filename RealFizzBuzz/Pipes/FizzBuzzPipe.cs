2eabcbbfd246b6d4c94e5b00312a5534c11634e5
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-06 19:47:21 +0100

    Added pipes

---
+++ RealFizzBuzz/Pipes/FizzBuzzPipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealFizzBuzz.Pipes
+{
+    public class FizzBuzzPipe : IPipe
+    {
+        public bool Check(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Process(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealFizzBuzz/Pipes/IPipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealFizzBuzz.Pipes
+{
+    public interface IPipe
+    {
+        bool Check(int number);
+
+        string Process(int number);
+    }
+}
