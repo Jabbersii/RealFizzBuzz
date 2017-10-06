401bf97b6fdc5703c745d0a47bcaf39b6c67a1a3
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-06 20:35:53 +0100

    Implemented CompositePipe

--- RealFizzBuzz/Pipes/CompositePipe.cs
+++ RealFizzBuzz/Pipes/CompositePipe.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RealFizzBuzz.Pipes
 {
     public class CompositePipe : IPipe
     {
+        private readonly IPipe[] pipes;
+
         public CompositePipe(params IPipe[] pipes)
         {
+            if (pipes.Length == 0)
+            {
+                throw new ArgumentException("Must specify 1 or more pipes", nameof(pipes));
+            }
 
+            this.pipes = pipes;
         }
 
         public bool Check(int number)
         {
-            throw new NotImplementedException();
+            return pipes.All(p => p.Check(number));
         }
 
         public string Process(int number)
         {
-            throw new NotImplementedException();
+            return pipes.Select(p => p.Process(number)).Aggregate((s1, s2) => s1 + s2);
         }
     }
 }
