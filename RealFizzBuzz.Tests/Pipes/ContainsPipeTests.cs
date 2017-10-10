db8e706068c716c27b5c0f5aa71224f9949989e0
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-10 12:46:44 +0100

    Added ContainsPipe outline. Added ContainsPipe tests

---
+++ RealFizzBuzz.Tests/Pipes/ContainsPipeTests.cs
@@ -0,0 +1,32 @@
+﻿using RealFizzBuzz.Pipes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RealFizzBuzz.Tests.Pipes
+{
+    public class ContainsPipeTests
+    {
+        [Theory]
+        [MemberData(nameof(CheckTestsData))]
+        public void Contains_pipe_check_passes_correctly(int input, bool expected)
+        {
+            IPipe pipe = new ContainsPipe(3, string.Empty);
+
+            Assert.Equal(expected, pipe.Check(input));
+        }
+
+        public static IEnumerable<object[]> CheckTestsData = new[]
+        {
+            new object[] { 3,   true  },
+            new object[] { 2,   false },
+            new object[] { 30,  true  },
+            new object[] { 20,  false },
+            new object[] { 32,  true  },
+            new object[] { 300, true  },
+            new object[] { 302, true  },
+            new object[] { 200, false }
+        };
+    }
+}
---
+++ RealFizzBuzz/Pipes/ContainsPipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealFizzBuzz.Pipes
+{
+    public class ContainsPipe : IPipe
+    {
+        public ContainsPipe(int target, string substitute)
+        {
+
+        }
+
+        public bool Check(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Process(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
