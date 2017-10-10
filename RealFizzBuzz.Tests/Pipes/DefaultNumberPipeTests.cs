a9891058f3643ab2840fe071fd87372f800af683
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-10 09:36:46 +0100

    Added DefaultNumberPipe and tests

---
+++ RealFizzBuzz.Tests/Pipes/DefaultNumberPipeTests.cs
@@ -0,0 +1,29 @@
+﻿using RealFizzBuzz.Pipes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RealFizzBuzz.Tests.Pipes
+{
+    public class DefaultNumberPipeTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Number_pipe_returns_ToString_result_on_input(int input, string expected)
+        {
+            IPipe pipe = new DefaultNumberPipe();
+            string actual = pipe.Process(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> TestData => new[]
+        {
+            new object[] { 1, "1" },
+            new object[] { 3, "3" },
+            new object[] { -1, "-1" },
+            new object[] { 0, "0" }
+        };
+    }
+}
