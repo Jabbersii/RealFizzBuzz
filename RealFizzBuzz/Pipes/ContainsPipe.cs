e59c2c92823127cfe423f4914090699ae33575ae
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-10 12:52:55 +0100

    Implemented ContainsPipe

--- RealFizzBuzz/Pipes/ContainsPipe.cs
+++ RealFizzBuzz/Pipes/ContainsPipe.cs
@@ -6,19 +6,32 @@ namespace RealFizzBuzz.Pipes
 {
     public class ContainsPipe : IPipe
     {
+        private readonly int target;
+        private readonly string substitute;
+
         public ContainsPipe(int target, string substitute)
         {
-
+            this.target = target;
+            this.substitute = substitute;
         }
 
         public bool Check(int number)
         {
-            throw new NotImplementedException();
+            while (number != 0)
+            {
+                if (number % 10 == target)
+                {
+                    return true;
+                }
+                else
+                {
+                    number = number / 10;
+                }
+            }
+
+            return false;
         }
 
-        public string Process(int number)
-        {
-            throw new NotImplementedException();
-        }
+        public string Process(int number) => this.substitute;
     }
 }
