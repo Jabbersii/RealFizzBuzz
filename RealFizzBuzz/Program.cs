39f2835872d7679e5aeaa66c5b7537f5698bf828
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-10 12:29:06 +0100

    Added Console FizzBuzz

--- RealFizzBuzz/Program.cs
+++ RealFizzBuzz/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RealFizzBuzz.Pipes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RealFizzBuzz
 {
@@ -6,7 +9,26 @@ namespace RealFizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fizz = new NumberSubstitutePipe(3, "fizz");
+            var buzz = new NumberSubstitutePipe(5, "buzz");
+            var fizzbuzz = new CompositePipe(fizz, buzz);
+            var defaultPipe = new DefaultNumberPipe();
+
+            List<IPipe> pipes = new List<IPipe>()
+            {
+                fizzbuzz,
+                fizz,
+                buzz,
+                defaultPipe
+            };
+
+            var runner = new Runner(Enumerable.Range(1, 20), pipes);
+            foreach (var item in runner.Run())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
