e34e150883776b3ce950ce9c79d482ba669d0abf
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-10 09:37:17 +0100

    Implemented NumberSubstitutePipe

--- RealFizzBuzz/Pipes/NumberSubstitutePipe.cs
+++ RealFizzBuzz/Pipes/NumberSubstitutePipe.cs
@@ -6,19 +6,17 @@ namespace RealFizzBuzz.Pipes
 {
     public class NumberSubstitutePipe : IPipe
     {
+        private readonly int target;
+        private readonly string substitute;
+
         public NumberSubstitutePipe(int target, string substitute)
         {
-
+            this.target = target;
+            this.substitute = substitute;
         }
 
-        public bool Check(int number)
-        {
-            throw new NotImplementedException();
-        }
+        public bool Check(int number) => number % this.target == 0;
 
-        public string Process(int number)
-        {
-            throw new NotImplementedException();
-        }
+        public string Process(int number) => this.substitute;
     }
 }
