23e6f8950e989b92afa9dad9a5495f49570cb378
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-10-10 12:15:32 +0100

    Implemented Runner

--- RealFizzBuzz/Runner.cs
+++ RealFizzBuzz/Runner.cs
@@ -1,20 +1,34 @@
 ﻿using RealFizzBuzz.Pipes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RealFizzBuzz
 {
     public class Runner
     {
+        private readonly IEnumerable<int> source;
+        private readonly IList<IPipe> pipes;
+
         public Runner(IEnumerable<int> source, IList<IPipe> pipes)
         {
+            if (pipes.Count == 0)
+            {
+                throw new ArgumentException("Must supply at least 1 pipe", nameof(pipes));
+            }
 
+            this.source = source;
+            this.pipes = pipes;
         }
 
         public IEnumerable<string> Run()
         {
-            return null;
+            foreach (var item in source)
+            {
+                var pipe = pipes.First(p => p.Check(item));
+                yield return pipe.Process(item);
+            }
         }
     }
 }
